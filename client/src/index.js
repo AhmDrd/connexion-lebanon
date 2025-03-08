5a7a740ce2e0958ae2ee4e3ad8e9bb973f97ae35
AhmdRd <ahmxd.m8@gmail.com>
2025-03-08 21:03:54 +0200

    ..

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import store from "./redux/store";
 import { Provider } from "react-redux";
-
+//Redux in root folder
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
